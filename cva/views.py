afae526f49624b51df829823f2d44751142ef63a
BlisS <contacto@fixter.org>
2016-05-20 16:16:17 -0500

    test

--- cva/views.py
+++ cva/views.py
@@ -36,40 +36,41 @@ class Home(View):
 
 class Indexar(View):
 	def get(self,request):
+		pass
 		# template="cva/home.html"
-		contador = 0
-		prods = 0
-		url = 'https://www.grupocva.com/catalogo_clientes_xml/lista_precios.xml?cliente=24808&marca=%25&grupo=%25&clave=%25&codigo=%25'
-		data = utils.pedido(url)
-		for item in data:
-			try:
-				p = Producto.objects.get(clave=item['clave'],descripcion=item['descripcion'])
-				print('Producto ya existe',p)
-				contador+=1
+		# contador = 0
+		# prods = 0
+		# url = 'https://www.grupocva.com/catalogo_clientes_xml/lista_precios.xml?cliente=24808&marca=%25&grupo=%25&clave=%25&codigo=%25'
+		# data = utils.pedido(url)
+		# for item in data:
+		# 	try:
+		# 		p = Producto.objects.get(clave=item['clave'],descripcion=item['descripcion'])
+		# 		print('Producto ya existe',p)
+		# 		contador+=1
 
-			except:
+		# 	except:
 
-				p = Producto()
+		# 		p = Producto()
 
-				p.clave = item['clave']
-				p.codigo_fabricante = item['codigo_fabricante']
-				p.descripcion = item['descripcion']
-				p.marca = item['marca']
-				p.garantia = item['garantia']
-				p.clase = item['clase']
-				p.disponible = item['disponible']
-				p.precio = item['precio']
-				p.moneda = item['moneda']
-				p.ficha_tecnica = item['ficha_tecnica']
-				p.ficha_comercial = item['ficha_comercial']
-				p.imagen = item['imagen']
-				p.disponibleCD = item['disponibleCD']
+		# 		p.clave = item['clave']
+		# 		p.codigo_fabricante = item['codigo_fabricante']
+		# 		p.descripcion = item['descripcion']
+		# 		p.marca = item['marca']
+		# 		p.garantia = item['garantia']
+		# 		p.clase = item['clase']
+		# 		p.disponible = item['disponible']
+		# 		p.precio = item['precio']
+		# 		p.moneda = item['moneda']
+		# 		p.ficha_tecnica = item['ficha_tecnica']
+		# 		p.ficha_comercial = item['ficha_comercial']
+		# 		p.imagen = item['imagen']
+		# 		p.disponibleCD = item['disponibleCD']
 
-				p.save()
-				prods+=1
+		# 		p.save()
+		# 		prods+=1
 
-		messages.warning(request,'Productos agregados: {} \n Productos repetidos: {}'.format(prods,contador))
-		return redirect('cva:home')
+		# messages.warning(request,'Productos agregados: {} \n Productos repetidos: {}'.format(prods,contador))
+		# return redirect('cva:home')
 
 
 
