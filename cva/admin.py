ec4e0695f01c855b3c93cf7c6a7e204f52973339
BlisS <contacto@fixter.org>
2016-05-20 23:49:03 -0500

    detallitos

--- cva/admin.py
+++ cva/admin.py
@@ -8,7 +8,7 @@ from import_export.admin import ImportExportMixin, ImportMixin, ExportActionMode
 
 
 class ProductoAdmin(ImportExportActionModelAdmin, ImportExportMixin, admin.ModelAdmin):
-	list_display = ('clave','codigo_fabricante','grupo','marca','clase','disponible','precio','moneda','disponibleCD','descripcion')
+	list_display = ('descripcion','marca','clase','disponible','precio','moneda','disponibleCD','clave','codigo_fabricante','grupo')
 	list_filter = ('marca','clase','moneda')
 	search_fields = ('descripcion','codigo_fabricante','clave','marca','garantia','ficha_tecnica','ficha_comercial')
 
